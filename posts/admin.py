f371ffe54eb1e407fdee27172225880dd9c01bc3
heshamsayed <hesham.sayed636@gmail.com>
2019-11-06 22:16:34 +0200

    feat : adding customization for posts in  admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title","updated","timestamp"]
+    list_display_links = ["updated"]
+    list_filter = ["updated","timestamp"]
+    search_fields = ["title","content"]
+    class meta:
+        model = Post
+
+admin.site.register(Post,PostModelAdmin)
